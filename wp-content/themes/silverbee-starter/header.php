642718ec6f38187ab2e8e1352099efbb7a13253d
Sander Geraedts <sander@silverbee.nl>
2017-11-06 22:43:46 +0100

    header work

--- wp-content/themes/silverbee-starter/header.php
+++ wp-content/themes/silverbee-starter/header.php
@@ -12,40 +12,45 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?> <span>/</span></span></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <header id="masthead" class="site-header" role="banner"
+            style="background-image: url('<?php echo get_template_directory_uri() . '/dist/img/header.png' ?>')">
+        <div class="container">
+            <div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            &lt;<?php bloginfo( 'name' ); ?> <span>/</span>&gt;
+                        </a>
+                    </h1>
+				<?php else : ?>
+                    <p class="site-title">
+                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                            &lt;<?php bloginfo( 'name' ); ?> <span>/</span>&gt;
+                        </a>
+                    </p>
+				<?php endif; ?>
+            </div><!-- .site-branding -->
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'silverbee-starter' ); ?></button>
-			<?php
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <button class="menu-toggle" aria-controls="primary-menu"
+                        aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'silverbee-starter' ); ?></button>
+				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				?>
+            </nav><!-- #site-navigation -->
+        </div>
+    </header>
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content container">
